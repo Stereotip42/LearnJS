45ed32a5301018d306a0c50473b29b90b58eb67d
Stereotip42 <stereotip22@gmail.com>
2020-03-02 17:57:07 +0300

    task2

---
+++ task2/js/app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const iconsDir = 'icons';
+    const iconExtension = 'gif';
+    const zombiesSelector = '.zombies';
+    const typeDeadZombie = 0;
+    const srcDeadZombie = `${iconsDir}/${typeDeadZombie}.${iconExtension}`;
+
+    function outputZombies() {
+        const zombieContainer = document.querySelector(zombiesSelector);
+
+        for (let zombieData of zombies) {
+            const zombieItem = document.createElement('img');
+            zombieItem.classList.add('zombie-item');
+
+            zombieItem.src = `${iconsDir}/${zombieData.type}.${iconExtension}`;
+
+            zombieContainer.appendChild(zombieItem);
+
+            zombieItem.addEventListener('click', function() {
+                this.src = srcDeadZombie;
+            });
+        }
+    }
+
+    outputZombies();
+});
