a78caecfb06277867ce94ce8093148b8e8f11462
Stereotip42 <stereotip22@gmail.com>
2020-03-02 18:45:24 +0300

    1.1, code review

--- task2/js/app.js
+++ task2/js/app.js
@@ -1,26 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let iconName = 'zombie-dead';
     const iconsDir = 'icons';
     const iconExtension = 'gif';
     const zombiesSelector = '.zombies';
-    const typeDeadZombie = 0;
-    const srcDeadZombie = `${iconsDir}/${typeDeadZombie}.${iconExtension}`;
+    const srcDeadZombie = `${iconsDir}/${iconName}.${iconExtension}`;
+    const zombieContainer = document.querySelector(zombiesSelector);
 
-    function outputZombies() {
-        const zombieContainer = document.querySelector(zombiesSelector);
+    for (let zombieData of zombies) {
+        const zombieItem = document.createElement('img');
+        zombieItem.classList.add('zombie-item');
 
-        for (let zombieData of zombies) {
-            const zombieItem = document.createElement('img');
-            zombieItem.classList.add('zombie-item');
-
-            zombieItem.src = `${iconsDir}/${zombieData.type}.${iconExtension}`;
-
-            zombieContainer.appendChild(zombieItem);
-
-            zombieItem.addEventListener('click', () => {
-                zombieItem.src = srcDeadZombie;
-            });
+        switch (zombieData.type) {
+            case ZOMBIE_TYPE.SMALL:
+                iconName = 'zombie-small';
+                break;
+            case ZOMBIE_TYPE.MAD:
+                iconName = 'zombie-mad';
+                break;
+            case ZOMBIE_TYPE.STRONG:
+                iconName = 'zombie-strong';
+                break;
+            default:
+                iconName = 'zombie-dead';
+                break;
         }
-    }
 
-    outputZombies();
+        zombieItem.src = `${iconsDir}/${iconName}.${iconExtension}`;
+
+        zombieContainer.appendChild(zombieItem);
+
+        zombieItem.addEventListener('click', () => {
+            zombieItem.src = srcDeadZombie;
+        });
+    }
 });
